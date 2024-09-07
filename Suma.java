7f619f32b276e4ce9fc083250708a84ab4d7864f
Jorge <yonemotoh.jorgeg@gmail.com>
2024-09-06 18:00:42 -0600

    Modificación de los archivos HolaMundo y Suma

--- Suma.java
+++ Suma.java
@@ -1,7 +1,17 @@
-class Suma{
+//Se crea la clase Suma
+
+class Suma {
+
+    //Se utiliza el método más importante(main)
     public static void main(String[] args) {
-        int a=6,b=9,r;
-        r= a+b;
+        /*Se declaran las variables con su tipo de dato 
+        y se les asisgnan valores(a r no se le asigna ningun 
+        valor porque es la variable que guardará el 
+        resultado de nuestra suma)*/
+        int a = 6, b = 9, r;
+        //Se realiza la suma y r almacena el resultado
+        r = a + b;
+        //Se imprime el resultado en pantalla
         System.out.println(r);
     }
 }
